2e996dabcdca0cc2a3899773a68e5a10e1110aab
Rafał Kadow <rafal.kadow@gmail.com>
2024-04-06 23:13:32 +0200

    Added controller PlcParameterHistory to the project.

---
+++ src/Web.Api/Controllers/PlcParameterHistory/PlcParameterHistoryController.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+using AutoMapper;
+using Domain.Interfaces;
+using Domain.Modules.PlcParameterHistory.Commands;
+using Domain.Modules.PlcParameterHistory.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Web.Api.Exceptions;
+
+namespace Web.Api.Controllers.V1
+{
+    //[Authorize]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PlcParameterHistoryController : BaseApiController<PlcParameterHistoryController>
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<GetPlcParameterHistoryResultAll>), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(BaseResponseDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get()
+        {
+            var query = new GetPlcParameterHistoryQueryAll();
+            var response = await mediator.Send(query);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(int), (int)HttpStatusCode.Created)]
+        [ProducesErrorResponseType(typeof(BaseResponseDTO))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Post([FromBody] CreatePlcParameterHistoryCommand command)
+        {
+            try
+            {
+                var response = await mediator.Send(command);
+                return StatusCode((int)HttpStatusCode.Created, response);
+            }
+            catch (InvalidRequestBodyException ex)
+            {
+                return BadRequest(new BaseResponseDTO
+                {
+                    IsSuccess = false,
+                    Errors = ex.Errors
+                });
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(GetPlcParameterHistoryResultById), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(BaseResponseDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                var query = new GetPlcParameterHistoryQueryById(id);
+                var response = await mediator.Send(query);
+                return Ok(response);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(new BaseResponseDTO
+                {
+                    IsSuccess = false,
+                    Errors = new string[] { ex.Message }
+                });
+            }
+        }
+
+
+    }
+}
