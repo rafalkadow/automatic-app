880bfb8770683c41b91cd35681127bc0d88b816c
Rafał Kadow <rafal.kadow@gmail.com>
2024-04-09 23:53:55 +0200

    Added controller PlcDriverAlarm to the project, 40th.

---
+++ src/Web.Api/Controllers/PlcDriverAlarm/PlcDriverAlarmController.cs
@@ -0,0 +1,79 @@
+﻿using Domain.Modules.PlcDriverAlarm.Commands;
+using Domain.Modules.PlcDriverAlarm.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Web.Api.Exceptions;
+
+namespace Web.Api.Controllers.V1
+{
+    //[Authorize]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PlcDriverAlarmController : BaseApiController<PlcDriverAlarmController>
+    {
+        private readonly ILogger<PlcDriverAlarmController> logger;
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<GetPlcDriverAlarmResultAll>), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(BaseResponseDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get()
+        {
+            var query = new GetPlcDriverAlarmQueryAll();
+            var response = await mediator.Send(query);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(int), (int)HttpStatusCode.Created)]
+        [ProducesErrorResponseType(typeof(BaseResponseDTO))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Post([FromBody] CreatePlcDriverAlarmCommand command)
+        {
+            try
+            {
+                var response = await mediator.Send(command);
+                return StatusCode((int)HttpStatusCode.Created, response);
+            }
+            catch (InvalidRequestBodyException ex)
+            {
+                return BadRequest(new BaseResponseDTO
+                {
+                    IsSuccess = false,
+                    Errors = ex.Errors
+                });
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(GetPlcDriverAlarmResultById), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(BaseResponseDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                var query = new GetPlcDriverAlarmQueryById(id);
+                var response = await mediator.Send(query);
+                return Ok(response);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(new BaseResponseDTO
+                {
+                    IsSuccess = false,
+                    Errors = new string[] { ex.Message }
+                });
+            }
+        }
+
+
+    }
+}
